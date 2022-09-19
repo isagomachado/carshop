733bdfea2ba5da4ccdbfb537e4f1f8157df95307
isagomachado <isa_gomes_machado@hotmail.com>
2022-09-19 17:33:29 -0300

    Requisito 2 - interface IVehicle

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+const vehicleZodSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().int().positive().min(1900)
+    .max(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
+});
+
+export type IVehicle = z.infer<typeof vehicleZodSchema>;
+
+export { vehicleZodSchema };
