8392e23387da08ea668ddb6cdec220db7dc645fd
isagomachado <isa_gomes_machado@hotmail.com>
2022-09-20 17:55:34 -0300

    Requisito 5 - feito - testes camada model

---
+++ src/tests/unit/models/car.test.ts
@@ -0,0 +1,26 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+
+import CarModel from '../../../models/Car';
+import { carMock, carMockWithId } from '../../mocks/carMock';
+
+describe('Car Model', () => {
+  const carModel = new CarModel();
+
+  beforeEach(() => {
+    sinon.stub(Model, 'create').resolves(carMockWithId)
+  })
+
+  afterEach(() => {
+		sinon.restore();
+	});
+
+  describe('creating a car', () => {
+    it('successfully created', async () => {
+      const newCar = await carModel.create(carMock);
+      expect(newCar).to.be.deep.equal(carMockWithId)
+    })
+  })
+
+})
